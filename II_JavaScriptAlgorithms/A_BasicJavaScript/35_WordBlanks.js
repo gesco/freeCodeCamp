f2e21261f5eeae4abc2232a7419ac8d334d89f0a
gesco <gescobedoz@gmail.com>
2018-07-12 09:51:14 -0500

    Using knowledge of strings to build a Mad Libs style word game called Word Blanks

---
+++ II_JavaScriptAlgorithms/A_BasicJavaScript/35_WordBlanks.js
@@ -0,0 +1,13 @@
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your code below this line
+  var result = "It was really " + myAdjective + ", and we " + myAdverb + " " +
+  myVerb + " like a " + myNoun;
+
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("cat", "little", "hit", "slowly");
+
+console.log(wordBlanks());
