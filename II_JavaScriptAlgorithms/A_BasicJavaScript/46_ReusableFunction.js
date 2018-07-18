71fd3a9f0d80fe3491eb018a18456f77fe488b09
gesco <gescobedoz@gmail.com>
2018-07-17 23:22:09 -0500

    Write Reusable JavaScript with Functions

---
+++ II_JavaScriptAlgorithms/A_BasicJavaScript/46_ReusableFunction.js
@@ -0,0 +1,16 @@
+// You can call or invoke this function by using its name followed by parentheses,
+// like this:
+
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+function reusableFunction() {
+  console.log("Hi World");
+}
+
+reusableFunction();
