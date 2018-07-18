39b9f9428e4b5f5bb477c637e90bf9500fab281a
gesco <gescobedoz@gmail.com>
2018-07-17 23:18:01 -0500

    Shopping List

---
+++ II_JavaScriptAlgorithms/A_BasicJavaScript/45_ShopList.js
@@ -0,0 +1,5 @@
+// Create a shopping list in the variable myList. The list should be a
+// multi-dimensional array containing several sub-arrays.
+
+var myList = [["Chocolate Bar", 15], ["Milk", 2.32], ["Bread", 1.12],
+["Tuna", 0.85], ["Apple", 0.55]];
