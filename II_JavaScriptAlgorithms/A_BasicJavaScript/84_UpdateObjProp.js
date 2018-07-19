7f93b3e83aa6b95be03f909735c2b092b12b59e0
gesco <gescobedoz@gmail.com>
2018-07-19 09:55:02 -0500

    Updating Object Properties

---
+++ II_JavaScriptAlgorithms/A_BasicJavaScript/84_UpdateObjProp.js
@@ -0,0 +1,24 @@
+// After you've created a JavaScript object, you can update its properties 
+// at any time just like you would update any other variable. You can use 
+// either dot or bracket notation to update.
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.name = "Happy Camper";
+
+// Setup
+var myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line.
+myDog.name = "Happy Coder";
