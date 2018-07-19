51d79c69710c47394d5c0dbe6290cffa1f6bc31f
gesco <gescobedoz@gmail.com>
2018-07-18 23:16:45 -0500

    Accessing Object Properties with Dot Notation

---
+++ II_JavaScriptAlgorithms/A_BasicJavaScript/81_AccessObj.js
@@ -0,0 +1,14 @@
+// There are two ways to access the properties of an object: dot notation (.) and
+// bracket notation ([]), similar to an array.
+
+// Setup
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+
+var hatValue = testObj.hat;      // Change this line
+var shirtValue = testObj.shirt;    // Change this line
