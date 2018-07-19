c1f1b8ce992885756a4c743695ead17b43e889f5
gesco <gescobedoz@gmail.com>
2018-07-18 23:20:22 -0500

    Accessing Object Properties with Bracket Notation

---
+++ II_JavaScriptAlgorithms/A_BasicJavaScript/82_ObjBracketNtn.js
@@ -0,0 +1,13 @@
+// The second way to access the properties of an object is bracket notation ([]).
+
+// Setup
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+
+var entreeValue = testObj["an entree"];   // Change this line
+var drinkValue = testObj["the drink"];    // Change this line
