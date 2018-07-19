ca10c3c4aa8b834d0a04f66730fa94602a5acedf
gesco <gescobedoz@gmail.com>
2018-07-18 23:13:52 -0500

    Build JavaScript Objects

---
+++ II_JavaScriptAlgorithms/A_BasicJavaScript/80_JSObjects.js
@@ -0,0 +1,20 @@
+// Objects are similar to arrays, except that instead of using indexes to access 
+// and modify their data, you access the data in objects through what are called
+// properties.
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "Honey",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Oreo", "Peanut", "Celcius"]
+};
