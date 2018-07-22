9ac14cc9677a10dfa6ebb9157f2ca512a624685e
gesco <gescobedoz@gmail.com>
2018-07-21 23:44:20 -0500

    Manipulating Complex Objects

---
+++ II_JavaScriptAlgorithms/A_BasicJavaScript/89_ManipulateCmplxObj.js
@@ -0,0 +1,32 @@
+// Sometimes you may want to store data in a flexible Data Structure. A JavaScript
+// object is one way to handle flexible data. They allow for arbitrary combinations
+// of strings, numbers, booleans, arrays, functions, and objects.
+
+// JavaScript Object Notation or JSON is a related data interchange format used
+// to store data.
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add record here
+  {
+    "artist": "Daft Punk",
+    "title": "Homework",
+    "release_year": 1997,
+    "formats": [
+      "CD",
+      "Cassette",
+      "LP"
+    ],
+    "gold": true
+  }
+];
