0cb3e64a038ee6e49cda57fd381eedb230fa0956
gesco <gescobedoz@gmail.com>
2018-07-21 23:45:47 -0500

    Accessing Nested Objects

---
+++ II_JavaScriptAlgorithms/A_BasicJavaScript/90_AcesNestedObj.js
@@ -0,0 +1,29 @@
+var ourStorage = {
+  "desk": {
+    "drawer": "stapler"
+  },
+  "cabinet": {
+    "top drawer": {
+      "folder1": "a file",
+      "folder2": "secrets"
+    },
+    "bottom drawer": "soda"
+  }
+};
+ourStorage.cabinet["top drawer"].folder2; // "secrets"
+ourStorage.desk.drawer; // "stapler"
+
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
