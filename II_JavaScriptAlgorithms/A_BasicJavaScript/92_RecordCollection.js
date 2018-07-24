1d5cd37fc284358a0c588cbc676b1bfb56a2a6ee
gesco <gescobedoz@gmail.com>
2018-07-24 18:27:45 -0500

    Record Collection

--- II_JavaScriptAlgorithms/A_BasicJavaScript/92_RecordCollection.js
+++ II_JavaScriptAlgorithms/A_BasicJavaScript/92_RecordCollection.js
@@ -1,3 +1,15 @@
+// You are given a JSON object representing a part of your musical album collection.
+// Each album has several properties and a unique id number as its key. Not all
+// albums have complete information.
+//
+// Write a function which takes an album's id (like 2548), a property prop
+// (like "artist" or "tracks"), and a value (like "Addicted to Love") to modify
+// the data in this collection.
+//
+// Your function must always return the entire collection object.
+//
+// There are several rules for handling incomplete data:
+
 // Setup
 var collection = {
     "2548": {
@@ -27,33 +39,52 @@ var collection = {
 // Keep a copy of the collection for tests
 // var collectionCopy = JSON.parse(JSON.stringify(collection));
 
+// If prop isn't "tracks" and value isn't empty (""), update or set the value for
+// that record album's property.
+//
+// If prop is "tracks" but the album doesn't have a "tracks" property, create an
+// empty array before adding the new value to the album's corresponding property.
+// If prop is "tracks" and value isn't empty (""), push the value onto the end of
+// the album's existing tracks array.
+//
+// If value is empty (""), delete the given prop property from the album.
+
 // Only change code below this line
 function updateRecords(id, prop, value) {
-  // If prop isn't "tracks" and value isn't empty (""), update or set the value
-  // for that record album's property.
+
   if (prop != "tracks" && value !== "") {
     collection[id][prop] = value;
-  }
-  // If prop is "tracks" but the album doesn't have a "tracks" property,
-  // create an empty array before adding the new value to the album's
-  // corresponding property.
-  if (prop === "tracks" && collection.hasOwnProperty(prop)){
-    collection[id][prop] = [];
-  }
-  // If prop is "tracks" and value isn't empty (""), push the value onto the end of
-  // the album's existing tracks array.
-  if (prop === "tracks" && value !== "") {
-    collection[id][prop].push(value);
-  }
-// If value is empty (""), delete the given prop property from the album
-  if (value === "") {
+  } else if (prop === "tracks" && value !== "") {
+      if (collection[id][prop]) {
+        collection[id][prop].push(value);
+      } else {
+          collection[id][prop] = [value];
+      }
+  } else {
     delete collection[id][prop];
   }
+
   return collection;
 }
 
 // Alter values below to test your code
-updateRecords(5439, "artist", "ABBA");
+//updateRecords(5439, "artist", "ABBA");
+// console.log(collection);
+// updateRecords(5439, "tracks", "Take a Chance on Me");
+// console.log(collection);
+updateRecords(2468, "tracks", "Free");
 console.log(collection);
-updateRecords(5439, "tracks", "Take a Chance on Me");
-console.log(collection);
+
+
+// if (prop != "tracks" && value !== "") {
+//   collection[id][prop] = value;
+// } else if (prop === "tracks" && value !== "") {
+//     if ((collection.hasOwnProperty(prop))) {
+//       collection[id][prop] = [];
+//       collection[id][prop].push(value);
+//     } else {
+//         collection[id][prop].push(value);
+//     }
+// } else {
+//   delete collection[id][prop];
+// }
