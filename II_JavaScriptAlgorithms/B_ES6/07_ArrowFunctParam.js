9b8847a441fcb0c6ec285558bd87b64bfb7edd3d
gesco <gescobedoz@gmail.com>
2018-08-02 19:07:36 -0500

    Write Arrow Functions with Parameters

---
+++ II_JavaScriptAlgorithms/B_ES6/07_ArrowFunctParam.js
@@ -0,0 +1,25 @@
+// Just like a normal function, you can pass arguments into arrow functions.
+
+// doubles input value and returns it
+// const doubler = (item) => item * 2;
+// You can pass more than one argument into arrow functions as well.
+//
+//
+// Rewrite the myConcat function which appends contents of arr2 to arr1 so that
+// the function uses arrow function syntax.
+
+// var myConcat = function(arr1, arr2) {
+//   "use strict";
+//   return arr1.concat(arr2);
+// };
+
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
+// test your code
+console.log(myConcat([1, 2], [3, 4, 5]));
+
+ let storeMyConcat = myConcat([1, 2], [3, 4, 5]);
+ console.log(storeMyConcat);
+
+// let storeMyConcat = [];
+// storeMyConcat = myConcat;
+// console.log(storeMyConcat);
