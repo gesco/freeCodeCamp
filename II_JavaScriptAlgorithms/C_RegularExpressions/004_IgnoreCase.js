b1675d16edfbed0bf9c780b1e5086a1178c8cf98
gesco <gescobedoz@gmail.com>
2019-01-23 22:15:53 -0600

    Ignore Case While Matching

---
+++ II_JavaScriptAlgorithms/C_RegularExpressions/004_IgnoreCase.js
@@ -0,0 +1,15 @@
+////////////////////////////////////////////////////////////////////////////////
+//             Regular Expressions: Ignore Case While Matching
+// You can match both cases using what is called a flag. There are other flags
+// but here you'll focus on the flag that ignores case - the i flag. You can use
+// it by appending it to the regex. An example of using this flag is
+// /ignorecase/i. This regex can match the strings "ignorecase", "igNoreCase",
+// and "IgnoreCase".
+////////////////////////////////////////////////////////////////////////////////
+let myString = "freeCodeCamp";
+let fccRegex = /freeCodeCamp/i; // Change this line
+let result = fccRegex.test(myString);
+console.log(result);  // returns true
+
+// The regex so that it detects “freeCodeCamp”, “FREECODECAMP”, and
+// “FrEeCoDeCaMp” using the i flag.
