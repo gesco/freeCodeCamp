1dfb8561d347c896074eae153815d5ad2fdc5068
gesco <gescobedoz@gmail.com>
2019-01-28 18:04:53 -0600

    Use an Array to Store a Collection of Data

---
+++ II_JavaScriptAlgorithms/E_BasicDataStructures/001_UseAnArray.js
@@ -0,0 +1,37 @@
+////////////////////////////////////////////////////////////////////////////////
+//     Basic Data Structures: Use an Array to Store a Collection of Data
+////////////////////////////////////////////////////////////////////////////////
+
+// let simpleArray = ['one', 2, 'three’, true, false, undefined, null];
+// console.log(simpleArray.length);  // logs 7
+
+// let complexArray = [
+//   [
+//     {
+//       one: 1,
+//       two: 2
+//     },
+//     {
+//       three: 3,
+//       four: 4
+//     }
+//   ],
+//   [
+//     {
+//       a: "a",
+//       b: "b"
+//     },
+//     {
+//       c: "c",
+//       d: “d”
+//     }
+//   ]
+// ];
+
+
+// Complete the statement by assigning an array of at least 5 elements in
+// length to the yourArray variable. Your array should contain at least one
+// string, one number, and one boolean.
+let yourArray = ["Coffee", 42, true, "Electronics", false ]; // change this line
+console.log(yourArray.length); // Returns 5
+console.log(yourArray);  // Returns [ "Coffee", 42, true, "Electronics", false ]
