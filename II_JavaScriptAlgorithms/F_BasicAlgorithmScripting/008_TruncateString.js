6e1d2f9530039b3adce4886c7fc36a99e74cd56c
gesco <gescobedoz@gmail.com>
2019-02-01 23:23:35 -0600

    Truncate a String

---
+++ II_JavaScriptAlgorithms/F_BasicAlgorithmScripting/008_TruncateString.js
@@ -0,0 +1,31 @@
+////////////////////////////////////////////////////////////////////////////////
+//             Basic Algorithm Scripting: Truncate a String                   //
+//                                                                            //
+// Truncate a string (first argument) if it is longer than the given maximum  //
+// string length (second argument). Return the truncated string with a        //
+// ... ending.                                                                //
+////////////////////////////////////////////////////////////////////////////////
+
+function truncateString(str, num) {
+  if (str.length > num && num > 3) {
+    return str.slice(0, (num - 3)) + '...';
+  } else if (str.length > num && num <= 3) {
+    return str.slice(0, num) + '...';
+  } else {
+    return str;
+  }
+}
+
+// truncateString("A-tisket a-tasket A green and yellow basket", 8);
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8));
+//should return "A-tisket...".
+console.log(truncateString("Peter Piper picked a peck of pickled peppers", 11));
+//should return "Peter Piper...".
+console.log(truncateString("A-tisket a-tasket A green and yellow basket",
+"A-tisket a-tasket A green and yellow basket"));
+// should return "A-tisket a-tasket A green and yellow basket".
+console.log(truncateString("A-tisket a-tasket A green and yellow basket",
+"A-tisket a-tasket A green and yellow basket".length + 2));
+// should return "A-tisket a-tasket A green and yellow basket".
+console.log(truncateString("A-", 1)); // should return "A...".
+console.log(truncateString("Absolutely Longer", 2)); //should return "Ab...".
