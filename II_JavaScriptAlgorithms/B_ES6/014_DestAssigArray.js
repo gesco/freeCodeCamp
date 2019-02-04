39f6d321bf19652276c583be1eeb361098aa787b
gesco <gescobedoz@gmail.com>
2019-02-04 16:36:14 -0600

    Use Destructuring Assignment to Assign Variables from Arrays

---
+++ II_JavaScriptAlgorithms/B_ES6/014_DestAssigArray.js
@@ -0,0 +1,17 @@
+// ES6: Use Destructuring Assignment to Assign Variables from Arrays
+
+// const [a, b] = [1, 2, 3, 4, 5, 6];
+// console.log(a, b); // 1, 2
+//
+// const [a, b,,, c] = [1, 2, 3, 4, 5, 6];
+// console.log(a, b, c); // 1, 2, 5
+
+let a = 8, b = 6;
+(() => {
+
+  // change code below this line
+  [b, a] = [a, b];
+  // change code above this line
+})();
+console.log(a); // should be 6
+console.log(b); // should be 8
