c96a168730ac9973684aa988a04055106fcc048d
gesco <gescobedoz@gmail.com>
2019-02-16 16:00:54 -0600

    Use export to Reuse a Code Block

---
+++ II_JavaScriptAlgorithms/B_ES6/023_Export.js
@@ -0,0 +1,14 @@
+////////////////////////////////////////////////////////////////////////////////
+//                ES6: Use export to Reuse a Code Block                       //
+////////////////////////////////////////////////////////////////////////////////
+
+/*
+Below are two variables that I want to make available for other files to use.
+Utilizing the first way I demonstrated export, export the two variables.
+*/
+
+export {capitalizeString};
+
+"use strict";
+export const foo = "bar";
+export const bar = "foo";
