d0f0b938a6c8257ed96e2f071fafbb81ea491543
gesco <gescobedoz@gmail.com>
2019-02-20 22:14:34 -0600

    Import a Default Export

---
+++ II_JavaScriptAlgorithms/B_ES6/026_Import.js
@@ -0,0 +1,10 @@
+////////////////////////////////////////////////////////////////////////////////
+//                      ES6: Import a Default Export                          //
+////////////////////////////////////////////////////////////////////////////////
+
+/*
+In the following code, please import the default export, subtract, from the file
+"math_functions", found in the same directory as this file.
+*/
+
+import subtract from "math_functions"
