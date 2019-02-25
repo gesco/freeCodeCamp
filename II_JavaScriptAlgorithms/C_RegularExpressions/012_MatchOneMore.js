b105a7fed251748585e6c3af31d8703ffc632845
gesco <gescobedoz@gmail.com>
2019-02-25 16:24:32 -0600

    Match Characters that Occur One or More Times

---
+++ II_JavaScriptAlgorithms/C_RegularExpressions/012_MatchOneMore.js
@@ -0,0 +1,8 @@
+////////////////////////////////////////////////////////////////////////////////
+//             Match Characters that Occur One or More Times
+////////////////////////////////////////////////////////////////////////////////
+
+let difficultSpelling = "Mississippi";
+let myRegex = /s+/g; // Change this line
+let result = difficultSpelling.match(myRegex);
+console.log(result);  // Returns [ 'ss', 'ss' ]
