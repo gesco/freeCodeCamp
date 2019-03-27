e967f64d7144fdf061bec0cfb4bdee5237cb31ac
gesco <gescobedoz@gmail.com>
2019-03-27 18:10:04 -0500

    Match All Numbers

---
+++ II_JavaScriptAlgorithms/C_RegularExpressions/020_AllNumbers.js
@@ -0,0 +1,13 @@
+////////////////////////////////////////////////////////////////////////////////
+//               Regular Expressions: Match All Numbers
+// The shortcut to look for digit characters is \d, with a lowercase d. This is
+// equal to the character class [0-9], which looks for a single character of any
+// number between zero and nine.
+////////////////////////////////////////////////////////////////////////////////
+
+// Use the shorthand character class \d to count how many digits are in movie
+// titles. Written out numbers ("six" instead of 6) do not count.
+let numString = "Your sandwich will be $5.00";
+let numRegex = /\d/g; // Change this line
+let result = numString.match(numRegex).length;
+console.log(result);  // Returns 3
